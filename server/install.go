7173177396b4e9cbdf56474d43b919382bb91026
404nova <stijn@numblio.com>
2026-06-06 00:12:50 +0200

    fix: Improve script execution logic in install function
    - Refactored the script execution condition in the install method to ensure scripts are only skipped during reinstallation when explicitly configured.
    - Enhanced clarity of the condition by introducing a local variable for better readability.

--- server/install.go
+++ server/install.go
@@ -36,7 +36,8 @@ func (s *Server) Install() error {
 
 func (s *Server) install(reinstall bool) error {
 	var err error
-	if !s.Config().SkipEggScripts {
+	skipScripts := s.Config().SkipEggScripts && !reinstall
+	if !skipScripts {
 		// Send the start event so the Panel can automatically update. We don't
 		// send this unless the process is actually going to run, otherwise all
 		// sorts of weird rapid UI behavior happens since there isn't an actual
